d6812528d9cce5c79f662e35c8efc6bfa18a6c57
Samuel Christopher <Ebusameric@gmail.com>
2019-06-29 15:13:21 +0100

    Updates

---
+++ precache-manifest.88543d50c5d45c284c173423feccad97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "852d890441444104344723770eb66ef8",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "e1fbcdf8ce9444b56508",
+    "url": "/random-quote-machine/static/css/main.d3631682.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/random-quote-machine/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "e1fbcdf8ce9444b56508",
+    "url": "/random-quote-machine/static/js/main.69f8f46e.chunk.js"
+  },
+  {
+    "revision": "6fefced6fd426e504746",
+    "url": "/random-quote-machine/static/js/runtime~main.296aa1cf.js"
+  }
+]);
